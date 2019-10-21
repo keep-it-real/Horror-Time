95fc8010c099e00b85100a58354d1d56cf7deae2
Dorota Kargul <dorota.kargul.89@gmail.com>
2019-10-21 21:34:42 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,19 @@
+window.onscroll = function()
+{
+	myFunction()
+}
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction()
+{
+	if(window.pageYOffset >= sticky)
+	{
+		nav.classList.add("sticky")
+	}	
+	else
+	{
+		nav.classList.remove("sticky");
+	}
+}
